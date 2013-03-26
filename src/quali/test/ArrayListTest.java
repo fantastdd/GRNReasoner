22be81468455a237da69333cb2d85cd1c25a834a
fantastdd <fantastdd@gmail.com>
2013-03-26 17:40:12 +1100

    neighbor

---
+++ src/quali/test/ArrayListTest.java
@@ -0,0 +1,23 @@
+package quali.test;
+
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import quali.MBR;
+
+public class ArrayListTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       LinkedList<MBR> list = new LinkedList<MBR>();
+       LinkedHashSet<MBR> set = new LinkedHashSet<MBR>();
+
+       //list.add(2, 1);
+       System.out.println(list.size());
+	}
+
+}
