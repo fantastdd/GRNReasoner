553b8235a061ff0ce54eba6a7aacd7a4546163e8
fantastdd <fantastdd@gmail.com>
2013-04-11 12:16:04 +1000

    add files

---
+++ src/quali/MBRRegisterVO.java
@@ -0,0 +1,64 @@
+package quali;
+
+import java.awt.Rectangle;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MBRRegisterVO {
+	private static LinkedList<MBR> mbrs = new LinkedList<MBR>();
+	private static LinkedList<MBR> edge = new LinkedList<MBR>();
+
+	public static LinkedList<MBR> getMbrs() {
+		return mbrs;
+	}
+
+	public static void setMbrs(LinkedList<MBR> mbrs) {
+		MBRRegisterVO.mbrs = mbrs;
+	}
+
+	private static int count = 0;
+
+	public static void registerRectangle(Rectangle rec, boolean edge) {
+		MBR mbr = new MBR(rec);
+		if (edge)
+			MBRRegisterVO.edge.add(mbr);
+		if (!mbrs.contains(mbr)) {
+			mbr.setId(count++);
+			mbrs.add(mbr);
+		}
+	}
+
+	public static void registerMBR(MBR mbr, boolean edge) {
+
+		if (edge)
+			MBRRegisterVO.edge.add(mbr);
+		if (!mbrs.contains(mbr)) {
+			mbr.setId(count++);
+			mbrs.add(mbr);
+		}
+	}
+
+	public static void batchRegister(List<MBR> mbrs, MBR... edge) {
+
+		for (MBR mbr : mbrs) {
+			boolean isEdge = false;
+			for (MBR _edge : edge) {
+				if (_edge.equals(mbr))
+					isEdge = true;
+
+			}
+			registerMBR(mbr, isEdge);
+		}
+
+	}
+
+	public static TestNode constructTestNode() {
+
+		TestNode node = new TestNode(mbrs, edge);
+
+		return node;
+
+	}
+
+	
+}
